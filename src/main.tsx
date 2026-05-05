a0335ee1541907c9cb2a81670433e0af78c88838
suleiman hamza <hamzasuleiman530@gmail.com>
2026-05-05 09:21:12 +0100

    hotfix: clear lint errors

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,6 @@ import { RouterProvider } from "react-router/dom";
 import { router } from "./router";
 import "./index.css";
 
-const root = document.getElementById("root");
+const root = document.getElementById("root")!;
 
 ReactDOM.createRoot(root).render(<RouterProvider router={router} />);
