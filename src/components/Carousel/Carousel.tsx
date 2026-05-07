f491cb6fb27f67d98f8e631bc6ef11580dd570df
suleiman hamza <hamzasuleiman530@gmail.com>
2026-05-07 11:57:06 +0100

    fix: enhance Carousel component layout and styling for improved user experience

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -8,11 +8,13 @@ export default function Carousel() {
   const goToPrev = () => emblaApi?.scrollPrev();
   const goToNext = () => emblaApi?.scrollNext();
   return (
-    <section className="border">
+    <section className="container border font-alonira py-12 md:py-18">
       <div className="embla">
-        <div className={styles.embla__viewport} ref={emblaRef}>
+        <div className={`${styles.embla__viewport} outline outline-blue-300 mb-4`} ref={emblaRef}>
           <div className={styles.embla__container}>
-            <div className={styles.embla__slide}>Wedding</div>
+            <div className={`${styles.embla__slide} p-2 border w-full h-auto`}>
+              <span>Wedding</span>
+            </div>
             <div className={styles.embla__slide}>Pop-up Dining</div>
             <div className={styles.embla__slide}>Private Chef</div>
             <div className={styles.embla__slide}>Bespoke Evevnt</div>
@@ -20,12 +22,14 @@ export default function Carousel() {
           </div>
         </div>
 
-        <button className="embla__prev" onClick={goToPrev}>
-          Back
-        </button>
-        <button className="embla__next" onClick={goToNext}>
-          Next
-        </button>
+        <div className="border border-red-400 flex justify-between">
+          <button className="embla__prev" onClick={goToPrev}>
+            Back
+          </button>
+          <button className="embla__next" onClick={goToNext}>
+            Next
+          </button>
+        </div>
       </div>
     </section>
   );
